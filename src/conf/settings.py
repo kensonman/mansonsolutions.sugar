b30e4270d3bd2e5ed20cf9f159dc894b76abbea0
Kenson Man <kenson@breakthrough.org.hk>
2017-09-15 21:09:19 +0800

    Add the locale

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -116,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh_hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Hong_Kong'
 
 USE_I18N = True
 
